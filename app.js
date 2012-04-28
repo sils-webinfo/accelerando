57c3148e26566aa85a9a6903ad91d91e1b78cfb8
ide user ide_colosbian <colosbian@yahoo.com>
2012-04-28 16:21:36 +0000

    Added some needed handlers.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 
-var USER_OR_GROUP_NAME = ''; // TODO: Insert GitHub username or group name.
+var USER_OR_GROUP_NAME = 'accelerando'; // TODO: Insert GitHub username or group name.
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -26,21 +26,51 @@ var db = utils.connectToDatabase(USER_OR_GROUP_NAME);
 // examples below, or you may need to add additional handlers by copying,
 // pasting, and modifying these examples.
 
+
+
 ////////////////////////////////////////////////////////////////////////////////
-// Example of handling PUT to create or update a resource. /////////////////////
-// Here we create or update an item using the ID specified in the URI. /////////
+// Handling PUT to update piece resource. /////////////////////
+// Here we update a piece using the ID specified in the URI. /////////
 ////////////////////////////////////////////////////////////////////////////////
-app.put('/parties/:id',      // TODO: change to suit your URI design.
+app.put('/pieces/:id',      // TODO: change to suit your URI design.
   function(req, res) {
   
     // Get the item ID from the URI.
     var item_id = req.params.id;
 
     // Get the item info that was PUT from the input form.
-    // See the form in `views/list-parties.ejs`.
+    // See the form in `views/list-pieces.ejs`.
     var item = req.body.item;
     
-    item.type = 'party'; // TODO: change to the type of item you want
+    item.type = 'piece'; // TODO: change to the type of item you want
+
+    // Save the new item to the database, specifying the ID.
+    db.save(item_id, item, function(err) {
+
+      // If there was a database error, return an error status.
+      if (err) { res.send(err, 500); } 
+      
+      // Otherwise, redirect back to the URI from which the form was submitted.
+      else { res.redirect('back' ); }
+    });
+  }
+);
+
+////////////////////////////////////////////////////////////////////////////////
+// Handling PUT to update piece resource. /////////////////////
+// Here we update a piece using the ID specified in the URI. /////////
+////////////////////////////////////////////////////////////////////////////////
+app.put('/cycle/:id',      // TODO: change to suit your URI design.
+  function(req, res) {
+  
+    // Get the item ID from the URI.
+    var item_id = req.params.id;
+
+    // Get the item info that was PUT from the input form.
+    // See the form in `views/list-cycles.ejs`.
+    var item = req.body.item;
+    
+    item.type = 'cycle'; // TODO: change to the type of item you want
 
     // Save the new item to the database, specifying the ID.
     db.save(item_id, item, function(err) {
@@ -56,12 +86,12 @@ app.put('/parties/:id',      // TODO: change to suit your URI design.
 
 ////////////////////////////////////////////////////////////////////////////////
 // Example of handling GET of a "collection" resource. /////////////////////////
-// Here we list all items of type `party`. /////////////////////////////////////
+// Here we list all items of type `cycle`. /////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-app.get('/parties/',         // TODO: change to suit your URI design. 
+app.get('/cycles/',         // TODO: change to suit your URI design. 
   function(req, res) {
 
-    var item_type = 'party'; // TODO: change to the type of item you want.
+    var item_type = 'cycle'; // TODO: change to the type of item you want.
 
     // Get all items of the specified type from the database.
     db.getAll(item_type, function(err, items) {
@@ -72,7 +102,33 @@ app.get('/parties/',         // TODO: change to suit your URI design.
       // Otherwise, use the returned data to render an HTML page.
       else {
         res.render(
-          'list-parties',   // TODO: change to the name of your HTML template.
+          'list-cycles',   // TODO: change to the name of your HTML template.
+          { items: items }
+        );
+      }
+    });
+  }
+);
+
+////////////////////////////////////////////////////////////////////////////////
+// Example of handling GET of a "collection" resource. /////////////////////////
+// Here we list all items of type `piece`. /////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+app.get('/pieces/',         // TODO: change to suit your URI design. 
+  function(req, res) {
+
+    var item_type = 'piece'; // TODO: change to the type of item you want.
+
+    // Get all items of the specified type from the database.
+    db.getAll(item_type, function(err, items) {
+
+      // If there was a database error, return an error status.
+      if (err) { res.send(err, 500); } 
+
+      // Otherwise, use the returned data to render an HTML page.
+      else {
+        res.render(
+          'list-pieces',   // TODO: change to the name of your HTML template.
           { items: items }
         );
       }
@@ -84,14 +140,39 @@ app.get('/parties/',         // TODO: change to suit your URI design.
 // Example of handling POST to create a resource. //////////////////////////////
 // Here we create an item and allow the ID to be created automatically. ////////
 ////////////////////////////////////////////////////////////////////////////////
-app.post('/candidates/', // TODO: change to suit your URI design.
+app.post('/pieces/', // TODO: change to suit your URI design.
   function(req, res) {
   
     // Get the item info that was POSTed from the input form.
-    // See the form in `views/one-party.ejs`.
+    // See the form in `views/one-cycle.ejs`.
     var item = req.body.item;
 
-    item.type = 'candidate'; // TODO: change to the type of item you want
+    item.type = 'piece'; // TODO: change to the type of item you want
+
+    // Save the new item to the database. (No ID specified, it will be created.)
+    db.save(item, function(err) {
+
+      // If there was a database error, return an error status.
+      if (err) { res.send(err, 500); } 
+      
+      // Otherwise, redirect back to the URI from which the form was submitted.
+      else { res.redirect('back' ); }
+    });
+  }
+);
+
+////////////////////////////////////////////////////////////////////////////////
+// Example of handling POST to create a resource. //////////////////////////////
+// Here we create an item and allow the ID to be created automatically. ////////
+////////////////////////////////////////////////////////////////////////////////
+app.post('/cycles/', // TODO: change to suit your URI design.
+  function(req, res) {
+  
+    // Get the item info that was POSTed from the input form.
+    // See the form in `views/list-cycle.ejs`.
+    var item = req.body.item;
+
+    item.type = 'cycle'; // TODO: change to the type of item you want
 
     // Save the new item to the database. (No ID specified, it will be created.)
     db.save(item, function(err) {
@@ -109,17 +190,45 @@ app.post('/candidates/', // TODO: change to suit your URI design.
 // Another example of handling PUT to update a resource. ///////////////////////
 // Here we update an item using the ID specified in the URI. ///////////////////
 ////////////////////////////////////////////////////////////////////////////////
-app.put('/candidates/:id', // TODO: change to suit your URI design.
+app.put('/pieces/:id', // TODO: change to suit your URI design.
   function(req, res) {
   
     // Get the item ID from the URI.
     var item_id = req.params.id;
 
     // Get the item info that was PUT from the input form.
-    // See the form in `views/one-candidate.ejs`.
+    // See the form in `views/one-piece.ejs`.
     var item = req.body.item;
 
-    item.type = 'candidate'; // TODO: change to the type of item you want
+    item.type = 'piece'; // TODO: change to the type of item you want
+
+    // Save the new item to the database, specifying the ID.
+    db.save(item_id, item, function(err) {
+
+      // If there was a database error, return an error status.
+      if (err) { res.send(err, 500); } 
+      
+      // Otherwise, redirect back to the URI from which the form was submitted.
+      else { res.redirect('back' ); }
+    });
+  }
+);
+
+////////////////////////////////////////////////////////////////////////////////
+// Another example of handling PUT to update a resource. ///////////////////////
+// Here we update an item using the ID specified in the URI. ///////////////////
+////////////////////////////////////////////////////////////////////////////////
+app.put('/cycles/:id', // TODO: change to suit your URI design.
+  function(req, res) {
+  
+    // Get the item ID from the URI.
+    var item_id = req.params.id;
+
+    // Get the item info that was PUT from the input form.
+    // See the form in `views/one-cycle.ejs`.
+    var item = req.body.item;
+
+    item.type = 'cycle'; // TODO: change to the type of item you want
 
     // Save the new item to the database, specifying the ID.
     db.save(item_id, item, function(err) {
@@ -137,10 +246,10 @@ app.put('/candidates/:id', // TODO: change to suit your URI design.
 // Another example of handling GET of a "collection" resource. /////////////////
 // This time we support filtering the list by some criteria (i.e. searching). //
 ////////////////////////////////////////////////////////////////////////////////
-app.get('/candidates/',          // TODO: change to suit your URI design. 
+app.get('/cycles/',          // TODO: change to suit your URI design. 
   function(req, res) {
 
-    var item_type = 'candidate'; // TODO: change to the type of item you want.
+    var item_type = 'piece'; // TODO: change to the type of item you want.
 
     // Get items of the specified type that match the query.
     db.getSome(item_type, req.query, function(err, items) {
@@ -151,7 +260,7 @@ app.get('/candidates/',          // TODO: change to suit your URI design.
       // Otherwise, use the returned data to render an HTML page.
       else {
         res.render(
-          'list-candidates', // TODO: change to the name of your HTML template.
+          'list-pieces', // TODO: change to the name of your HTML template.
           { items: items }
         );
       }
@@ -164,10 +273,10 @@ app.get('/candidates/',          // TODO: change to suit your URI design.
 // This handler is more complicated, because we want to show not only the //////
 // item requested, but also links to a set of related items. ///////////////////
 ////////////////////////////////////////////////////////////////////////////////
-app.get('/parties/:id',      // TODO: change to suit your URI design.
+app.get('/cycles/:id',      // TODO: change to suit your URI design.
   function(req, res) {
 
-    var item_type = 'party'; // TODO: change to the type of item you want.
+    var item_type = 'cycle'; // TODO: change to the type of item you want.
 
     // Get the item ID from the URI.
     var item_id = req.params.id;
@@ -184,10 +293,10 @@ app.get('/parties/:id',      // TODO: change to suit your URI design.
       // Otherwise, get the related items associated with this item.
       else {
         
-        var related_type = 'candidate'; // TODO: change to type of related item.
+        var related_type = 'piece'; // TODO: change to type of related item.
 
         // Set our query to find the items related to the requested item.
-        req.query.party = item_id; // TODO: change `party` to reflect the
+        req.query.cycle = item_id; // TODO: change `party` to reflect the
                                    // relation between the item fetched above
                                    // and the related items to be fetched below.
 
@@ -200,7 +309,7 @@ app.get('/parties/:id',      // TODO: change to suit your URI design.
           // Otherwise, use the returned data to render an HTML page.
           else {
             res.render(
-            'one-party', // TODO: change to the name of your HTML template.
+            'one-cycle', // TODO: change to the name of your HTML template.
               { item: item, related_items: items }
             );
           }
@@ -216,10 +325,10 @@ app.get('/parties/:id',      // TODO: change to suit your URI design.
 // item requested, but also a list of potential related items, so that users ///
 // can select from a list when updating the item. //////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-app.get('/candidates/:id',       // TODO: change to suit your URI design.
+app.get('/pieces/:id',       // TODO: change to suit your URI design.
   function(req, res) {
 
-    var item_type = 'candidate'; // TODO: change to the type of item you want.
+    var item_type = 'piece'; // TODO: change to the type of item you want.
 
     // Get the item ID from the URI.
     var item_id = req.params.id;
@@ -236,7 +345,7 @@ app.get('/candidates/:id',       // TODO: change to suit your URI design.
       // Otherwise, get the items potentially related to this item.
       else {
         
-        var related_type = 'party'; // TODO: change to type of related item.
+        var related_type = 'cycle'; // TODO: change to type of related item.
 
         // Get all items of the specified related type.
         db.getAll(related_type, function(err, items) {
@@ -247,7 +356,7 @@ app.get('/candidates/:id',       // TODO: change to suit your URI design.
           // Otherwise, use the returned data to render an HTML page.
           else {
             res.render(
-              'one-candidate', // TODO: change to name of your HTML template.
+              'one-piece', // TODO: change to name of your HTML template.
               { item: item, related_items: items }
             );
           }
